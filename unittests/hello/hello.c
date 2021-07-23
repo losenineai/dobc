d993dc10bf09133cdcffb0da22d12a576d8f3bf6
baikaishiuc <baikaishiuc@gmail.com>
2021-07-24 00:11:39 +0800

    UPDATE:ollvm对哪些函数是否生成混淆似乎是随机性的

--- unittests/hello/hello.c
+++ unittests/hello/hello.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-char * hello(char *text, char *outbuf)
+char * hello(char *text, char *outbuf) __attribute((__annotate__(("bcf,fla,sub"))))
 {
     sprintf(outbuf, "hello, %s", text);
 
